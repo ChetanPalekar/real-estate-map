e3fb325f7ff0e2e641778e635f01c31419d6fc58
Chetan Palekar <palekarchetan1996@gmail.com>
2025-06-22 00:25:18 +0530

    Fix disclaimer popup and refresh button functionality

---
+++ config.js
@@ -0,0 +1,9 @@
+// Mapbox Configuration
+// Replace this with your actual Mapbox access token
+// You can get one for free at https://account.mapbox.com/
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicGFsZWthcmNoZXRhbjE5OTYiLCJhIjoiY21jNmR0aHRwMGZ5MTJtcDltcnp0NDF0cyJ9.d1UegAPbiQ2BrVka9A3Pw';
+
+// Export for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { MAPBOX_ACCESS_TOKEN };
+} 
