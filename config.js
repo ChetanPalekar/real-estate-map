3a0792480841abc89532d1eff2aac9a6acdc5aba
Chetan Palekar <palekarchetan1996@gmail.com>
2025-06-22 00:35:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Mapbox Configuration
 // Replace this with your actual Mapbox access token
 // You can get one for free at https://account.mapbox.com/
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicGFsZWthcmNoZXRhbjE5OTYiLCJhIjoiY21jNmR0aHRwMGZ5MTJtcDltcnp0NDF0cyJ9.d1UegAPbiQ2BrVka9A3Pw';
+const MAPBOX_ACCESS_TOKEN = 'MAPBOX_ACCESS_TOKEN';
 
 // Export for use in other files
 if (typeof module !== 'undefined' && module.exports) {
